c830b0232a6bf3200adc7703ef00c567aae4d164
Lina <linaroettgen@freenet.de>
2025-10-28 17:52:18 +0100

    initial commit

---
+++ src/projekt2package/p2class.java
@@ -0,0 +1,17 @@
+package projekt2package;
+
+import java.util.Scanner;
+
+public class p2class {
+
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+		String zeile = new String();
+		zeile = s.nextLine();
+		System.out.println(zeile);
+		s.close();
+
+	}
+
+}
